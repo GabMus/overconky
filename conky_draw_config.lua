2507051d11d9f51aaf9287c77e228827ef41772e
Gabriele Musco <gabmus@Trinity-Zero.localdomain>
2017-05-06 14:33:35 +0200

    new overhauled version

--- conky_draw_config.lua
+++ conky_draw_config.lua
@@ -7,41 +7,41 @@ elements = {
     { --cpu ring
         kind = 'ring_graph',
         conky_value = 'cpu cpu0',
-        center = {x = 60, y = 240},
-        radius = 30,
+        center = {x = 229, y = 175},
+        radius = 50,
 
-        background_color = 0x3977d5,
+        background_color = 0x00F088,
         background_alpha = 0.5,
-        background_thickness = 8,
+        background_thickness = 10,
 
-        bar_color = 0x3977d5,
+        bar_color = 0x00F088,
         bar_alpha = 1,
-        bar_thickness = 8,
+        bar_thickness = 10,
 
-        start_angle = 120,
+        start_angle = 179,
         end_angle = 360,
         
-        background_thickness_critical = 8,
+        background_thickness_critical = 10,
         background_color_critical = 0xFF0000,
-        bar_thickness_critical = 8,
+        bar_thickness_critical = 10,
         bar_color_critical = 0xFF0000
     },
     
     { --memory ring
         kind = 'ring_graph',
         conky_value = 'memperc',
-        center = {x = 170, y = 240},
-        radius = 30,
+        center = {x = 86, y = 130},
+        radius = 20,
 
-        background_color = 0x3977d5,
+        background_color = 0x00F088,
         background_alpha = 0.5,
         background_thickness = 8,
 
-        bar_color = 0x3977d5,
+        bar_color = 0x00F088,
         bar_alpha = 1,
         bar_thickness = 8,
 
-        start_angle = 120,
+        start_angle = 0,
         end_angle = 360,
         
         background_thickness_critical = 8,
@@ -53,47 +53,47 @@ elements = {
     { --rootfs ring
         kind = 'ring_graph',
         conky_value = 'fs_used_perc /',
-        center = {x = 280, y = 240},
-        radius = 30,
+        center = {x = 366, y = 130},
+        radius = 20,
 
-        background_color = 0x3977d5,
+        background_color = 0x00F088,
         background_alpha = 0.5,
         background_thickness = 8,
 
-        bar_color = 0x3977d5,
+        bar_color = 0x00F088,
         bar_alpha = 1,
         bar_thickness = 8,
 
-        start_angle = 120,
-        end_angle = 360,
+        start_angle = 90,
+        end_angle = 270,
         
         background_thickness_critical = 8,
-        background_color_critical = 0x3977d5,
+        background_color_critical = 0x00F088,
         bar_thickness_critical = 8,
-        bar_color_critical = 0x3977d5
+        bar_color_critical = 0x00F088
     },
     
     { --homefs ring
         kind = 'ring_graph',
         conky_value = 'fs_used_perc /home',
-        center = {x = 390, y = 240},
-        radius = 30,
+        center = {x = 368, y = 130},
+        radius = 20,
 
-        background_color = 0x3977d5,
+        background_color = 0x00F088,
         background_alpha = 0.5,
         background_alpha_critical = 0.5,
         background_thickness = 8,
 
-        bar_color = 0x3977d5,
+        bar_color = 0x00F088,
         bar_alpha = 1,
         bar_thickness = 8,
 
-        start_angle = 120,
-        end_angle = 360,
+        start_angle = 90,
+        end_angle = -90,
         
         background_thickness_critical = 8,
-        background_color_critical = 0x3977d5,
+        background_color_critical = 0x00F088,
         bar_thickness_critical = 8,
-        bar_color_critical = 0x3977d5
+        bar_color_critical = 0x00F088
     },
 }
